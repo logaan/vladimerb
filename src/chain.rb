b6d7537329c1f8d0d53ff7ecc84d3c5cc7ba9584
Logan Campbell-McPherson <colin@logaan.net>
2012-06-18 20:10:11 +1000

    added chain implementation

---
+++ src/chain.rb
@@ -0,0 +1,11 @@
+class Chain
+  def initialize validation_1, validation_2
+    @validation_1 = validation_1
+    @validation_2 = validation_2
+  end
+
+  def validate record
+    error_1 = @validation_1.validate(record)
+    error_1.empty? ? @validation_2.validate(record) : error_1
+  end
+end
