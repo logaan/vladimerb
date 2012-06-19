e84cc506cd8d8346a79440feda79dbf3bfabde01
yongpeishi <pjong@thoughtworks.com>
2012-06-19 16:05:53 +1000

    implemented greater_than

---
+++ src/greater_than.rb
@@ -0,0 +1,12 @@
+class GreaterThan
+  def initialize field_name, minimum
+    @field_name = field_name
+    @minimum = minimum
+  end
+
+  def validate record
+    record.send(@field_name) > @minimum ?
+      [] :
+      [@field_name.to_s + " must be over " + @minimum.to_s + "." ]
+  end
+end
