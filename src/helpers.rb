77aac1a4f444edd12b9358e6b456464069825855
yongpeishi <pjong@thoughtworks.com>
2012-06-19 21:59:43 +1000

    added helpers methods for chain and greater than

--- src/helpers.rb
+++ src/helpers.rb
@@ -1,12 +1,22 @@
 require File.dirname(File.expand_path(__FILE__)) + "/join.rb"
+require File.dirname(File.expand_path(__FILE__)) + "/chain.rb"
 require File.dirname(File.expand_path(__FILE__)) + "/required.rb"
+require File.dirname(File.expand_path(__FILE__)) + "/greater_than.rb"
 
 module Helpers
   def join validation_1, validation_2
     Join.new validation_1,validation_2
   end
 
+  def chain validation_1, validation_2
+    Chain.new validation_1,validation_2
+  end
+
   def required field_name
     Required.new field_name
+  end
+
+  def greater_than field_name, minimum
+    GreaterThan.new field_name, minimum
   end
 end
--- test/helpers.rb
+++ test/helpers.rb
@@ -5,12 +5,22 @@ class HelpersTest < Test::Unit::TestCase
   include Helpers
 
   def test_can_build_composite_validation_attractively
+    age_validation = chain required(:age),
+                          greater_than(:age, 40)
     validation = join required(:name),
-                      required(:age)
+                      age_validation
 
     candidate = Candidate.new("Fred", 50)
     errors = validation.validate(candidate)
     assert_equal([], errors)
+
+    candidate = Candidate.new("Fred", 30)
+    errors = validation.validate(candidate)
+    assert_equal(["age must be over 40."], errors)
+
+    candidate = Candidate.new("", 50)
+    errors = validation.validate(candidate)
+    assert_equal(["name is required."], errors)
 
     candidate = Candidate.new(nil, nil)
     errors = validation.validate(candidate)
