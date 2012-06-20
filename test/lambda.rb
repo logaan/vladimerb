623028e376533c209fd78260146a21f83db8a195
yongpeishi <pjong@thoughtworks.com>
2012-06-20 11:49:57 +1000

    fix test

--- test/lambda.rb
+++ test/lambda.rb
@@ -5,9 +5,9 @@ require File.dirname(File.expand_path(__FILE__)) + "/../src/lambda.rb"
 class LambdaTest < Test::Unit::TestCase
   def tests_against_lambda_and_fails
     assert_errors Candidate.new(nil, 24),
-                  LambdaValidation.new do |record|
+                  (LambdaValidation.new do |record|
                     record.name.nil? ? ["Name is required."] : []
-                  end,
+                  end),
                   ["Name is required."]
   end
 end
