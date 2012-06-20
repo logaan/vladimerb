1c08a6a04b765b325a58af27242972147f5fbc78
yongpeishi <pjong@thoughtworks.com>
2012-06-20 10:56:43 +1000

    implemented Required and Pattern with custom message

--- src/pattern.rb
+++ src/pattern.rb
@@ -1,12 +1,21 @@
 class Pattern
-  def initialize field_name, regex
+  def initialize field_name, regex, options=nil
     @field_name = field_name
     @regex = regex
+    @custom_message = options[:message] unless options.nil?
   end
 
   def validate record
     record.send(@field_name).match(@regex).nil? ?
-      [@field_name.to_s + " is not formatted correctly."] :
+      [error_message] :
       []
+  end
+
+  private
+
+  def error_message
+    @custom_message.nil? ?
+      @field_name.to_s + " is not formatted correctly." :
+      @custom_message
   end
 end
--- src/required.rb
+++ src/required.rb
@@ -1,14 +1,21 @@
 class Required
-  def initialize field_name
+  def initialize field_name, options=nil
     @field_name = field_name
+    @custom_message = options[:message] unless options.nil?
   end
 
   def validate record
     data = record.send(@field_name)
-    not_provided?(data) ? [@field_name.to_s + " is required."] : []
+    not_provided?(data) ? [error_message] : []
   end
 
   private
+  def error_message
+    @custom_message.nil? ?
+      @field_name.to_s + " is required." :
+      @custom_message
+  end
+
   def not_provided? data
     data.nil? or is_empty?(data)
   end
